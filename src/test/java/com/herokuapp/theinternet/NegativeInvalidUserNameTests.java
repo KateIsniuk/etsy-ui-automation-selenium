47becb128b34c83bb53c884802002577b7360120
KateIsniuk <53911918+KateIsniuk@users.noreply.github.com>
2023-09-02 22:16:45 +0200

    Update NegativeInvalidUserNameTests.java

--- src/test/java/com/herokuapp/theinternet/NegativeInvalidUserNameTests.java
+++ src/test/java/com/herokuapp/theinternet/NegativeInvalidUserNameTests.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
