31719be617503fd29bb9e9e7aedf9eae98a80a8a
katerynaisniuk <Kate Isniuk katerina.isniuk@gmail.com>
2023-09-03 16:38:54 +0200

    TestNG (Parameters)

--- src/test/java/com/herokuapp/theinternet/NegativeTests.java
+++ src/test/java/com/herokuapp/theinternet/NegativeTests.java
@@ -5,12 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class NegativeTests {
     private String expectedMessage;
 
-    @Test (priority = 2, groups = { "negariveTests"})
+   /* @Test (priority = 2, groups = { "negariveTests"})
 
     public void invalidPassword() {
         System.out.println("Test is started");
@@ -52,9 +53,11 @@ public class NegativeTests {
         driver.quit();
         System.out.println("Test is finished");
 
-    }
-    @Test (priority = 1, groups = { "negariveTests", "smokeTests" })
-    public void invalidUserName() {
+    }*/
+
+    @Parameters({"username", "password", "expectedMessage"})
+    @Test
+    public void negativeLoginTest(String username, String password, String expectedErrorMessage) {
         System.out.println("Test is started");
 
         // Create driver
@@ -71,13 +74,13 @@ public class NegativeTests {
         System.out.println("Page is opened");
 
         // Enter username
-        WebElement username = driver.findElement(By.id("username"));
-        username.sendKeys("invalid user name");
+        WebElement usernameElement = driver.findElement(By.id("username"));
+        usernameElement.sendKeys(username);
         System.out.println("User name is entered");
 
         // Enter password
-        WebElement password = driver.findElement(By.name("password"));
-        password.sendKeys("SuperSecretPassword!");
+        WebElement passwordElement = driver.findElement(By.name("password"));
+        passwordElement.sendKeys(password);
         System.out.println("Password is entered");
 
         //Click the login button
@@ -86,11 +89,11 @@ public class NegativeTests {
 
         // verification
         WebElement errorMessage = driver.findElement(By.xpath("//div[@id='flash']"));
-        String expectedMessage = "Your username is invalid!";
+
         String actualMessage = errorMessage.getText();
 
-        Assert.assertTrue(actualMessage.contains(expectedMessage), "Actual message does not contain expected message.\nActual Message: "
-                + actualMessage + "\nExpected Message: " + expectedMessage);
+        Assert.assertTrue(actualMessage.contains(expectedErrorMessage), "Actual message does not contain expected message.\nActual Message: "
+                + actualMessage + "\nExpected Message: " + expectedErrorMessage);
 
         driver.quit();
         System.out.println("Test is finished");
