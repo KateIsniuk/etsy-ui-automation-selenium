1868865b3d97f25c15ad846fa55417ca55b60f3c
katerynaisniuk <Kate Isniuk katerina.isniuk@gmail.com>
2023-09-03 20:03:27 +0200

    TestNG join positive & negative tests

--- src/test/java/com/herokuapp/theinternet/loginTests.java
+++ src/test/java/com/herokuapp/theinternet/loginTests.java
@@ -5,16 +5,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import static org.openqa.selenium.By.*;
-
-public class PositiveTests {
+public class loginTests {
 
     private String expectedMessage;
 
-    @Test
-    public void loginTest() {
+    @Test (priority = 1, groups = {"positiveTests", "smokeTests"})
+    public void positiveLoginTest() {
         System.out.println("Test is started");
 
         // Create driver
@@ -63,6 +62,51 @@ public class PositiveTests {
 
         driver.close();
         System.out.println("Test is finished");
+    }
+
+    @Parameters({"username", "password", "expectedMessage"})
+    @Test (priority = 2, groups = {"negativeTests", "smokeTests"})
+    public void negativeLoginTest(String username, String password, String expectedErrorMessage) {
+        System.out.println("Test is started");
+
+        // Create driver
+
+        WebDriver driver = new ChromeDriver();
+        System.out.println("Browser is started");
+
+        // Open tested page
+        String url = "http://the-internet.herokuapp.com/login";
+        driver.get(url);
+
+        driver.manage().window().maximize();
+
+        System.out.println("Page is opened");
+
+        // Enter username
+        WebElement usernameElement = driver.findElement(By.id("username"));
+        usernameElement.sendKeys(username);
+        System.out.println("User name is entered");
+
+        // Enter password
+        WebElement passwordElement = driver.findElement(By.name("password"));
+        passwordElement.sendKeys(password);
+        System.out.println("Password is entered");
+
+        //Click the login button
+        WebElement logInButton = driver.findElement(By.tagName("button"));
+        logInButton.click();
+
+        // verification
+        WebElement errorMessage = driver.findElement(By.xpath("//div[@id='flash']"));
+
+        String actualMessage = errorMessage.getText();
+
+        Assert.assertTrue(actualMessage.contains(expectedErrorMessage), "Actual message does not contain expected message.\nActual Message: "
+                + actualMessage + "\nExpected Message: " + expectedErrorMessage);
+
+        driver.quit();
+        System.out.println("Test is finished");
+
     }
 
     /**
