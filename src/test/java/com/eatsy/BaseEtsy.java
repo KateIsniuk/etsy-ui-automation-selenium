5c94d7e9095708c6e99384583706c919539d4404
katerynaisniuk <Kate Isniuk katerina.isniuk@gmail.com>
2023-09-15 15:06:06 +0200

    test for filtering added and Base class created for before & after classes

---
+++ src/test/java/com/eatsy/BaseEtsy.java
@@ -0,0 +1,28 @@
+package com.eatsy;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+public class BaseEtsy {
+    private static final Logger logger = Logger.getLogger(BeforeClass.class.getName());
+    public WebDriver driver;
+    @BeforeClass
+    public void setUp(){
+        driver = new ChromeDriver();
+        String url = "https://www.etsy.com/";
+        driver.get(url);
+        driver.manage().window().maximize();
+        logger.log(Level.INFO,"Page is opened");
+    }
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+        logger.log(Level.INFO, "Test is finished");
+    }
+}
