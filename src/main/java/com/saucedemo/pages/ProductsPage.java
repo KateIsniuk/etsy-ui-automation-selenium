f16e46fdcf91b65fc913ee752a447746c79343e3
katerynaisniuk <Kate Isniuk katerina.isniuk@gmail.com>
2023-10-20 13:09:18 +0200

    Product Page - Refactor

--- src/main/java/com/saucedemo/pages/ProductsPage.java
+++ src/main/java/com/saucedemo/pages/ProductsPage.java
@@ -12,19 +12,21 @@ public class ProductsPage {
 
     private final WebDriverWait wait;
     private final WebDriver driver;
-
+    private static final long DEFAULT_WAIT_PERIOD = 10;
+    private static final By PRODUCT_ITEM_LOCATOR = By.xpath("//button[contains(text(), 'Add to cart')]");
+    private static final By SHOPPING_CART_LOCATOR = By.cssSelector(".shopping_cart_link");
     public ProductsPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_WAIT_PERIOD));
     }
 
     public void selectProductItems(String item01, String item02) {
-        WebElement item = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[contains(text(), 'Add to cart')]")));
+        WebElement item = wait.until(ExpectedConditions.presenceOfElementLocated(PRODUCT_ITEM_LOCATOR));
         item.click();
     }
 
     public void goToShoppingCart() {
-        WebElement card = driver.findElement(By.cssSelector(".shopping_cart_link"));
+        WebElement card = driver.findElement(SHOPPING_CART_LOCATOR);
         card.click();
     }
 }
