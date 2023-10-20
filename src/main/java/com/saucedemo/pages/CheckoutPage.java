cced06e7d01bc58deed6cf5c718a2a6b8107ad60
katerynaisniuk <Kate Isniuk katerina.isniuk@gmail.com>
2023-10-20 13:40:36 +0200

    Pages - Refactor

--- src/main/java/com/saucedemo/pages/CheckoutPage.java
+++ src/main/java/com/saucedemo/pages/CheckoutPage.java
@@ -5,38 +5,45 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class CheckoutPage {
+    private final WebDriver driver;
+
+    private static final By CHECKOUT_BUTTON_LOCATOR = By.id("checkout");
+    private static final By CONTINUE_BUTTON_LOCATOR = By.id("continue");
+    private static final By FINISH_ORDER_BUTTON_LOCATOR = By.id("finish");
+    private static final By CANCEL_ORDER_BUTTON_LOCATOR = By.id("cancel");
+    private static final By FIRST_NAME_FIELD_LOCATOR = By.id("first-name");
+    private static final By LAST_NAME_FIELD_LOCATOR = By.id("last-name");
+    private static final By ZIPCODE_FIELD_LOCATOR = By.id("postal-code");
     public CheckoutPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    private final WebDriver driver;
-
     public void clickCheckoutButton() {
-        WebElement checkout = driver.findElement(By.id("checkout"));
+        WebElement checkout = driver.findElement(CHECKOUT_BUTTON_LOCATOR);
         checkout.click();
     }
 
     public void fillInForm(String firstName, String lastName, String zipCode) {
-        WebElement firstNameField = driver.findElement(By.id("first-name"));
+        WebElement firstNameField = driver.findElement(FIRST_NAME_FIELD_LOCATOR);
         firstNameField.sendKeys(firstName);
 
-        WebElement lastNameField = driver.findElement(By.id("last-name"));
+        WebElement lastNameField = driver.findElement(LAST_NAME_FIELD_LOCATOR);
         lastNameField.sendKeys(lastName);
 
-        WebElement zipCodeField = driver.findElement(By.id("postal-code"));
+        WebElement zipCodeField = driver.findElement(ZIPCODE_FIELD_LOCATOR);
         zipCodeField.sendKeys(zipCode);
     }
 
     public void clickContinueButton() {
-        WebElement continueButton = driver.findElement(By.id("continue"));
+        WebElement continueButton = driver.findElement(CONTINUE_BUTTON_LOCATOR);
         continueButton.click();
     }
     public void finishOrder() {
-        WebElement finishButton = driver.findElement(By.id("finish"));
+        WebElement finishButton = driver.findElement(FINISH_ORDER_BUTTON_LOCATOR);
         finishButton.click();
     }
     public void cancelOrder() {
-        WebElement cancelButton = driver.findElement(By.id("cancel"));
+        WebElement cancelButton = driver.findElement(CANCEL_ORDER_BUTTON_LOCATOR);
         cancelButton.click();
     }
 
--- src/main/java/com/saucedemo/pages/LoginPage.java
+++ src/main/java/com/saucedemo/pages/LoginPage.java
@@ -8,20 +8,22 @@ import org.openqa.selenium.WebElement;
 public class LoginPage {
 
     private final WebDriver driver;
-
+    private static final By USER_NAME_FIELD_LOCATOR = By.id("user-name");
+    private static final By PASSWORD_FIELD_LOCATOR = By.id("password");
+    private static final By LOGIN_BUTTON_LOCATOR = By.id("login-button");
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public void login(String userName, String password) {
 
-        WebElement userNameField = driver.findElement(By.id("user-name"));
+        WebElement userNameField = driver.findElement(USER_NAME_FIELD_LOCATOR);
         userNameField.sendKeys(userName);
 
-        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement passwordField = driver.findElement(PASSWORD_FIELD_LOCATOR);
         passwordField.sendKeys(password);
 
-        WebElement loginButton = driver.findElement(By.id("login-button"));
+        WebElement loginButton = driver.findElement(LOGIN_BUTTON_LOCATOR);
         loginButton.click();
     }
 }
