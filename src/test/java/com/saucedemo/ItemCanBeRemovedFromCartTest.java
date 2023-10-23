bf88d99b6618ca599fc9485a90378319befd5750
KateIsniuk <53911918+KateIsniuk@users.noreply.github.com>
2023-10-23 11:29:07 +0200

    Update ItemCanBeRemovedFromCartTest.java

--- src/test/java/com/saucedemo/ItemCanBeRemovedFromCartTest.java
+++ src/test/java/com/saucedemo/ItemCanBeRemovedFromCartTest.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ItemCanBeRemovedFromCartTest extends BasicSaucedemoTest {
+   
     @Test
     public void shouldSuccessfullyProceedWithCheckoutWhenItemRemovedFromCart() {
 
