c9fe9b5747edc5b24c333135a3dd7a706a301a9f
KateIsniuk <53911918+KateIsniuk@users.noreply.github.com>
2024-01-17 19:35:11 +0100

    Update UserCreationDateValidation.java

--- src/test/java/com/reqres/UserCreationDateValidation.java
+++ src/test/java/com/reqres/UserCreationDateValidation.java
@@ -17,27 +17,27 @@ public class UserCreationDateValidation {
     @Test
     public void userCreationDateValidation() {
         String endpoint = "https://reqres.in/api/users";
-        String body = """
+       
+	//Create a new user
+	    String body = """
                 {
                   "name": "morpheus",
                   "job": "leader"
                 }
                 """;
-
         // Send the request and capture the response
         var response =
                 given().body(body)
                         .when()
                         .post(endpoint)
                         .then();
-
         // Log the response body
         response.log().body();
 
-        // Validate the response code
+        // Validate that the response code is `201`
         response.assertThat().statusCode(201);
 
-        // Validate the 'createdAt' field format
+        // Validate that the creation date is today's
         response.assertThat().body("createdAt", containsString("2024-01-17"));
 
     }
